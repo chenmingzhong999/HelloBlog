fceb849c72cd6a78f966e0d769de14777067fe19
xiaoming <xiaoming>
2020-09-13 21:41:01 +0800

    2222

--- src/main/java/net/evercodig/helloblog/controller/IndexController.java
+++ src/main/java/net/evercodig/helloblog/controller/IndexController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
     @RequestMapping(method = RequestMethod.GET)
     public String index(){
-        return "index11111";
+        return "index1111122222";
     }
 }
